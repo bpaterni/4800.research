cd1aeb1a468df63f32be0f8ba2d206257b11d78b
Brian Paterni <bpaterni@uni.edu>
2016-01-14 12:54:42 -0600

    include/: add centralized 'reasearch' header for use in tests

---
+++ include/research.h
@@ -0,0 +1,17 @@
+#ifndef __CSBS_RESEARCH_H
+#define __CSBS_RESEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glib.h>
+#include <glib/gstdio.h>
+
+#include <CL/cl.h>
+
+#include "bmp-utils.h"
+#include "gold.h"
+
+#include "utils.h"
+
+#endif  /* __CSBS_RESEARCH_h */
